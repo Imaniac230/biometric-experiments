0032b83b460a2545f0262b598b50848312b05ed6
Imaniac230 <44968160+Imaniac230@users.noreply.github.com>
2021-07-21 22:29:57 +0200

    run script from any drectory

--- makesharedlib.sh
+++ makesharedlib.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-gcc -Wall -Werror -pthread -fpic -shared -o R503_fingerprint.so R503_fingerprint.c R503_fingerprint.h -lpigpio -lrt
+#https://stackoverflow.com/questions/59895/how-can-i-get-the-source-directory-of-a-bash-script-from-within-the-script-itsel/246128
+path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+gcc -Wall -Werror -pthread -fpic -shared -o $path/R503_fingerprint.so $path/R503_fingerprint.c $path/R503_fingerprint.h -lpigpio -lrt
